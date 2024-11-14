d068839b3dce4da79486eb221259df2df8de8abf
wj <lwj0506@naver.com>
2024-11-14 11:25:15 +0900

    AuthInterceptor.kt 파일 주석 추가

--- src/main/kotlin/pack/config/AuthInterceptor.kt
+++ src/main/kotlin/pack/config/AuthInterceptor.kt
@@ -15,19 +15,20 @@ class AuthInterceptor: HandlerInterceptor {
     @Autowired
     private lateinit var jwtService: JwtService
 
+    // 컨트롤러 실행 전 JWT 인증 확인
+    // @NoAuth 애노테이션이 있으면 인증 생략
     override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
         val hm = handler as HandlerMethod
         val check: Boolean = hm.hasMethodAnnotation(NoAuth::class.java)
-        if(check) return true
+        if (check) return true
 
         return try {
-            val userIdInJwt = jwtService.getUserId()
-            request.setAttribute("userId", userIdInJwt)
+            val userIdInJwt = jwtService.getUserId()  // JWT에서 사용자 ID 추출
+            request.setAttribute("userId", userIdInJwt)  // 사용자 ID를 요청 속성에 추가
             true
-        } catch (e: Exception){
-            //
+        } catch (e: Exception) {
             println(e.message)
-            response.status = HttpServletResponse.SC_UNAUTHORIZED
+            response.status = HttpServletResponse.SC_UNAUTHORIZED  // 인증 실패 시 401 응답
             false
         }
     }
